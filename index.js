e949d6f72c5270fdeffc140d0d5bdef7513b0105
AtiqurRahmanAtik <atikahmedtanvir84@gmail.com>
2026-09-13 00:06:33 +0600

    feat:update single user data apicreate

--- index.js
+++ index.js
@@ -77,6 +77,88 @@ app.get("/users/:id", async(req,res)=>{
 })
 
 
+// update single user data
+app.put("/users/:id", async (req,res)=>{
+  const {id} = req.params;
+  const {name,email,password} = req.body;
+
+   if (!name || !email || !password)  {
+    return res.status(400).json({
+      error: "Name and email are required",
+    });
+  }
+
+  try{
+
+
+    const result = await db.query(
+      `UPDATE users 
+      SET name= $1, 
+      email = $2 ,
+      password = $3
+      
+     WHERE id = $4
+       RETURNING id, name, email, password`,
+      [name, email, password, id]
+     
+    )
+
+
+     if (result.rows.length === 0) {
+      return res.status(404).json({
+        error: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "User updated successfully",
+      user: result.rows[0],
+    });
+    // res.status(200).json({message:"update successfully",data:result.rows[0]})
+
+  }
+  catch(err){
+     console.error("Get users error:", err.message);
+
+    res.status(500).json({
+      error: "Server error while fetching users",
+    });
+  }
+})
+
+
+app.delete("/users/:id", async(req,res)=>{
+  const {id} = req.params;
+
+  try{
+
+    const result = await db.query(`
+      DELETE FROM users 
+      WHERE id = $1 
+      RETURNING id,name,email,password`,
+      [id]);
+
+    if(result.rows.length === 0){
+       return res.status(404).json({
+        error: "User not found",});
+    }
+
+    res.status(200).json({
+      message: "User deleted successfully",
+      user: result.rows[0]
+    });
+
+  }
+   catch (error) {
+    console.error("Delete user error:", error.message);
+
+    res.status(500).json({
+      error: "Server error while deleting user",
+    });
+  }
+})
+
+
 
 
 app.get('/', (req, res) => {
